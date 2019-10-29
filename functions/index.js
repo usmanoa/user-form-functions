e1ad7d8fabb7ae337b6d7991e2a7e80707bdbd80
usmanoa <usmanakorede@gmail.com>
2019-10-29 03:25:04 +0100

    Fixed addUserId function

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,33 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const cors = require('cors')({origin: true})
 const uuidv5 = require('uuid/v5');
 
 admin.initializeApp();
 
 exports.addUser = functions.https.onRequest(async (req, res) => {
-    const user = req.body;
-    const snapshot = await admin.database().ref('/users').push(user);
-    res.redirect(303, snapshot.ref.toString());
+    cors(req, res, ()=> {
+      const user = req.body;
+      admin.database().ref('/users').push(user);
+      res.status(200).send("Request Succesful")
+    })
   });
 
-  exports.getUsers = functions.https.onRequest(async (req, res) => {
+
+exports.getUsers = functions.https.onRequest(async (req, res) => {
+  cors(req, res, () => {
     return admin.database().ref('/users').on("value", snapshot => {
-        return res.status(200).send(snapshot.val());
+      return res.status(200).send(snapshot.val());
     }, error => {
-        console.error(error);
-        return response.status(500).send('Oh no! Error: ' + error);
+      console.error(error);
+      return response.status(500).send(error);
     });
-  });
+  })
+});
 
-exports.addUserId = functions.database.ref('/users/{pushId}/age')
+exports.addUserId = functions.database.ref('/users/{pushId}/userId')
   .onCreate((snapshot, context) => {
-    const name = snapshot.val().lastName
-    const userId = uuidv5(name, uuidv5.URL);
+    let userId = uuidv5('user' + Date.now(), uuidv5.URL);
     return snapshot.ref.parent.child('userId').set(userId);
   });
 
